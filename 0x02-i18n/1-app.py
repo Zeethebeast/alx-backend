e418115b293b2eef157d4fb85a74c90802172f3b
Zee <chimasamuel6@gmail.com>
2024-11-06 18:23:54 +0100

    i18n

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+from flask_babel import Babel
+
+
+class Config:
+    LANGUAGES =['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+app.config.from_object(Config) 
+babel = Babel(app)
+
+@app.route('/')
+def home():
+    return render_template('1-index.html')
+
+# def get_locale():
+#     return request.accept_languages.best_match(app.config['LANGUAGES'])
