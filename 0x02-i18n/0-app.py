5cdc83a2e809958542886e7e4747acfb871e6047
Zee <chimasamuel6@gmail.com>
2024-11-06 18:33:46 +0100

    i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,12 +2,12 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-message = "Hello World"
-title = "Welcome to Holberton"
+# message = "Hello World"
+# title = "Welcome to Holberton"
 
 @app.route('/')
 def home():
-    return render_template('index.html', message=message, title=title)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
