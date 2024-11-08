25bfa2b466bb401fa297abdc1a27d95a0aad3d9f
Zee <chimasamuel6@gmail.com>
2024-11-08 17:57:19 +0100

    logging and resub

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,21 +1,53 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request
+"""
+This module sets up a simple Flask application with
+internationalization support using Flask-Babel.
+"""
 
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config:
-    LANGUAGES =['en', 'fr']
+    """
+    Config class to store language settings for Flask-Babel.
+    
+    Attributes:
+        LANGUAGES (list): Supported languages.
+        BABEL_DEFAULT_LOCALE (str): Default locale for the app.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone for the app.
+    """
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app = Flask(__name__)
-app.config.from_object(Config) 
+app.config.from_object(Config)
 babel = Babel(app)
+
 
 @app.route('/')
 def home():
+    """
+    Render the home page template.
+
+    Returns:
+        Rendered HTML template for the home page.
+    """
     return render_template('1-index.html')
 
-# def get_locale():
-#     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match for supported languages based on request.
+
+    Returns:
+        str: Best matched language for the request.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
