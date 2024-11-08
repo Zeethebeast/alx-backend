6698f4930c0d60976f174325caf4a45b0cbe4a0b
zeethebeast <chimasamuel6@gmail.com>
2024-11-08 18:41:05 +0000

    debug0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def home():
-	""" home or root route """
-	return render_template('0-index.html')
+    """ home or root route """
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
